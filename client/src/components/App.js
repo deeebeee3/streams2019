550a83a8fc8cda70bd88c24b6c6301b473fb347c
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-05-10 13:02:25 +0100

    component scaffolding - setting up basic components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,22 +2,27 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 const PageOne = () => {
-    return <div>Page One</div>
+  return <div>Page One</div>;
 };
 
 const PageTwo = () => {
-    return <div>Page Two</div>
+  return <div>Page Two</div>;
 };
 
 const App = () => {
   return (
-      <BrowserRouter>
-        <div>
-            <Route path="/" exact component={PageOne} />
-            <Route path="/pagetwo" component={PageTwo} />
-        </div>
-      </BrowserRouter>
-  )
+    <BrowserRouter>
+      <div>
+        <Route path="/" exact={true} component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
+
+/* when it comes to deployment on a traditional server will need to configure it to 
+work with BrowserRouter (our create-react-app dev server always serves up index.html)  
+as its a single page app, other wise will get issues with app not behaving / navigating 
+properly */
