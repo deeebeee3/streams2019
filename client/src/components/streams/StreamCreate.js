8e002f2068f164b9d333879ebf16cb8912ed9ec5
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-05-12 12:00:45 +0100

    handling form submission with redux form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -11,16 +11,26 @@ class StreamCreate extends React.Component {
     );
   }
 
+  onSubmit(formValues) {
+      //now dont get useless event object which we only needed so we could preventDefault
+      //this works because our onSubmit is wrapped with redux forms handleSubmit function
+      console.log(formValues);
+  }
+
   render() {
     //console.log(this.props);
     return (
-      <form className="ui form">
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
           name="description"
           component={this.renderInput}
           label="Enter Description"
         />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
