22bd5e81246039ed15e2a3a174c0b60967d6639c
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-05-19 13:21:44 +0100

    merging a list of records

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _ from "lodash";
 import {
   FETCH_STREAM,
   FETCH_STREAMS,
@@ -9,6 +9,8 @@ import {
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
@@ -17,7 +19,6 @@ export default (state = {}, action) => {
       return { ...state, [action.payload.id]: action.payload };
     case DELETE_STREAM:
       return _.omit(state, action.payload);
-
     default:
       return state;
   }
