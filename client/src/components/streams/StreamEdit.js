f9c4ffea92746077bec52b604499b0876437f2c8
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-06-02 14:07:01 +0100

    refactor StreamEdit to use reusable StreamForm

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,31 +1,36 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
-  render() {
-    console.log(this.props); //props has to be this.props if class based component
+  onSubmit = (formValues) => {
+      console.log(formValues);
 
+  }
+
+  render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    return (
+        <div>
+            <h3>Edit a Stream</h3>
+            <StreamForm onSubmit={this.onSubmit} />
+        </div>
+    );
   }
 }
 
 const mapStateTopProps = (state, ownProps) => {
-  //ownProps is the same props object that shows up inside StreamEdit component
-  //console.log(ownProps);
-
-  //give our props object the stream
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
 export default connect(
   mapStateTopProps,
-  { fetchStream }
+  { fetchStream, editStream }
 )(StreamEdit);
