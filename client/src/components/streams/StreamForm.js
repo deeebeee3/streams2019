d5ea310632751d899776ee69463aa61ff4e83f53
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-06-02 13:49:34 +0100

    create a generalised StreamForm component to handle logic for creating and editing streams. Code was copied from StreamCreate.js into StreamForm.js and refactored to make it generalised.

--- client/src/components/streams/StreamForm.js
+++ client/src/components/streams/StreamForm.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
-import { connect } from "react-redux";
-import { createStream } from "../../actions";
 
-class StreamCreate extends React.Component {
+class StreamForm extends React.Component {
   renderError({ error, touched }) {
     if (touched && error) {
       return (
@@ -34,7 +32,7 @@ class StreamCreate extends React.Component {
     //this works because our onSubmit is wrapped with redux forms handleSubmit function
     //console.log(formValues);
 
-    this.props.createStream(formValues);
+    this.props.onSubmit(formValues);
   };
 
   render() {
@@ -71,12 +69,7 @@ const validate = formValues => {
   return errors;
 };
 
-const formWrapped = reduxForm({
-  form: "streamCreate",
+export default reduxForm({
+  form: "streamForm",
   validate: validate
-})(StreamCreate);
-
-export default connect(
-  null,
-  { createStream }
-)(formWrapped);
+})(StreamForm);
