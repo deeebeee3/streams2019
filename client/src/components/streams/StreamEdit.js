d8e734ef352b79232fb04f541394901a9bd1dd31
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-06-02 13:01:41 +0100

    selecting records from state

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const StreamEdit = () => {
-    return <div>StreamEdit</div>
+const StreamEdit = props => {
+  console.log(props);
+  return <div>StreamEdit</div>;
 };
 
-export default StreamEdit;
+const mapStateTopProps = (state, ownProps) => {
+  //ownProps is the same props object that shows up inside StreamEdit component
+  //console.log(ownProps);
+
+  //give our props object the stream
+  return {
+    stream: { stream: state.streams[ownProps.match.params.id] }
+  };
+};
+
+export default connect(mapStateTopProps)(StreamEdit);
