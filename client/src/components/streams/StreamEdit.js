6327637ad0cc30d2a871127aadc7a6c84e63eec0
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-06-02 13:32:57 +0100

    fetching a stream for Edit Stream and fixing silly error in how object returned from mapStateToProps

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,19 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamEdit = props => {
-  console.log(props);
-  return <div>StreamEdit</div>;
-};
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    console.log(this.props); //props has to be this.props if class based component
+
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.props.stream.title}</div>;
+  }
+}
 
 const mapStateTopProps = (state, ownProps) => {
   //ownProps is the same props object that shows up inside StreamEdit component
   //console.log(ownProps);
 
   //give our props object the stream
-  return {
-    stream: { stream: state.streams[ownProps.match.params.id] }
-  };
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateTopProps)(StreamEdit);
+export default connect(
+  mapStateTopProps,
+  { fetchStream }
+)(StreamEdit);
